a246ab2341401c2770a687ed71758f1df11cded7
katiemarriner <kemarriner@gmail.com>
2021-04-27 13:21:50 -0400

    new google analytics tag

--- bake.js
+++ bake.js
@@ -26,6 +26,16 @@ var source = `<div class="col-sm-8 col-12">
 </div>`
 
 var meta = `<title>Katie Marriner, data journalist and developer</title>
+<!--TRACKING-->	
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-37362223-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-37362223-3');
+</script>
 <meta name="description" content="Katie Marriner is data journalist and developer at MarketWatch in New York.">
 <meta name="author" content="Katie Marriner">
 <meta charset="UTF-8"/>
@@ -35,18 +45,7 @@ var meta = `<title>Katie Marriner, data journalist and developer</title>
 
 <link rel="shortcut icon" href="images/logo.ico" type="image/x-icon">
 <link href="dist/css/bootstrap-grid.min.css" rel="stylesheet" media="screen">
-<link href="dist/css/app.min.css" rel="stylesheet" media="screen">
-
-<!--TRACKING-->	
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-37362223-3', 'katiemarriner.com');
-ga('send', 'pageview');
-</script>`
+<link href="dist/css/app.min.css" rel="stylesheet" media="screen">`
 
 var nav = `<div class="col-sm-4 col-12">
   <ul class="vertical-nav full">
